025cda33db5579eb2446d27e793153cc932c3e48
Thomas <pavan_thomas@yahoo.com>
2022-07-29 18:35:11 -0700

    updated index

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ const IndexPage = () => {
   return (
     <main>
       <title>Home Page</title>
-      <h1>Welcome to my Gatsby site!</h1>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <h1>Welcome to the GitHub workflow demo</h1>
+      <p>This is a landing page</p>
     </main>
   )
 }
